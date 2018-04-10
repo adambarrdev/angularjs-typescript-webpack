2e5bc83859d4cebb3efaf725f6f7f9bf626f66e5
adambarrdev <adam@adambarrdev.net>
2018-04-09 22:04:41 -0400

    Some formatting

--- src/nav/nav.component.ts
+++ src/nav/nav.component.ts
@@ -20,7 +20,9 @@ export const NavComponent = {
     template: `
         <nav class="navbar navbar-default">
             <div class="navbar-header">
-                <button type="button" ng-click="vm.isCollapsed = !vm.isCollapsed" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <button type="button" ng-click="vm.isCollapsed = !vm.isCollapsed" 
+                        class="navbar-toggle collapsed" data-toggle="collapse" 
+                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -29,11 +31,14 @@ export const NavComponent = {
                 <a class="navbar-brand" href="#"><i class="fa fa-code"></i></a>
             </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
             <div uib-collapse="vm.isCollapsed" class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li ng-class="{active:vm.$state.includes('home')}"><a ui-sref="home">Home <span class="sr-only">(current)</span></a></li>
-                    <li ng-class="{active:vm.$state.includes('about')}"><a ui-sref="about">About</a></li>
+                    <li ng-class="{active:vm.$state.includes('home')}">
+                        <a ui-sref="home">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li ng-class="{active:vm.$state.includes('about')}">
+                        <a ui-sref="about">About</a>
+                    </li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="https://github.com/adambarrdev/angularjs-typescript-webpack" target="_blank"><i class="fa fa-github"></i></a></li>
